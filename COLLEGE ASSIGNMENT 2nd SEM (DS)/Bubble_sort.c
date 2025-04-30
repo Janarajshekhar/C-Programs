3e43ed63e739aa88612a35b66c849dc64c79dc4f
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-04-30 10:59:45 +0530

    Add implementations for Bubble Sort and Find Max/Min in Array

---
+++ COLLEGE ASSIGNMENT 2nd SEM (DS)/Bubble_sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void sort(int a[],int n);
+int main()
+{
+    int a[50],n,i,j,temp;
+    printf("\n Enter how many number : ");
+    scanf("%d",&n);
+    printf("\n Enter %d number : ",n);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    sort(a,n);
+}
+void sort(int a[],int n)
+{
+    int i,j,temp;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    printf("\n Sorted array is : ");
+    for(i=0;i<n;i++)
+        printf("%d  ",a[i]);
+}
---
+++ COLLEGE ASSIGNMENT 2nd SEM (DS)/find_max_min_in_array.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void find_max_min(int a[],int n);
+int main()
+{
+    int a[50],n,i,max,min;
+    printf("\n Enter how many number : ");
+    scanf("%d",&n);
+    printf("\n Enter %d number : ",n);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    find_max_min(a,n);
+}
+void find_max_min(int a[],int n)
+{
+    int max,min,i;
+    max=a[0];
+    min=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+        if(min>a[i])
+        {
+            min=a[i];
+        }
+    }
+    printf("\n Max = %d",max);
+    printf("\n min = %d",min);
+}
